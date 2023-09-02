da262d54494a2af66099d7b8e4f9b5cb43978fa4
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-02 18:49:20 +0330

    fetch menu loader

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,27 @@
-function Menu() {
-  return <h1>Menu</h1>;
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
+// fetch menu data 
+export async function loader() {
+    const menu = await getMenu();
+    return menu;
 }
 
+function Menu() {
+    // get the data from loader function 
+    const menu = useLoaderData()
+    console.log(menu)
+    // ui
+    return <ul>
+        {menu.map(pizza => <MenuItem pizza={pizza} key={pizza.id} />)}
+    </ul>
+}
+
+
+
+
 export default Menu;
+
+
+
