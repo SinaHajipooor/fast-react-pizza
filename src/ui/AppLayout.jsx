af356e327395d6b8e012c67239f52fa9db74d8ef
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-02 19:00:46 +0330

    display spinner while the app is laoding

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigation } from "react-router-dom"
 import CartOverview from "../features/cart/CartOverview"
 import Header from "./Header"
+import Spinner from "./Spinner";
 
 function AppLayout() {
+    // get the navigation data 
+    const navigation = useNavigation();
+    // to know if the compoenent is loaduing something or not 
+    const isLoading = navigation.state === 'loading';
+    // ui
     return (
-        <div>
+        <div className="layout">
+            {isLoading && <Spinner />}
             <Header />
             <main>
                 <Outlet />
