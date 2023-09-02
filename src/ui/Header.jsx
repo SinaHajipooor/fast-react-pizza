c0d3a6cf24fed771b8bf816530a29e7567179660
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-02 18:29:30 +0330

    build app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <header>
+            <Link to='/'>Fast React Pizza Company</Link>
+            <p>Sina Hajipour</p>
+        </header>
+    )
+}
+
+export default Header
