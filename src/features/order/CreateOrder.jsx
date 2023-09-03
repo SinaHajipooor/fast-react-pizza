961c84473a861ed5fff992c562cf3df7b2ec3db1
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-03 09:24:04 +0330

    optimize create order

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Form, redirect, useActionData, useNavigate } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
@@ -8,29 +7,29 @@ const isValidPhone = (str) =>
         str
     );
 
-const fakeCart = [
-    {
-        pizzaId: 12,
-        name: "Mediterranean",
-        quantity: 2,
-        unitPrice: 16,
-        totalPrice: 32,
-    },
-    {
-        pizzaId: 6,
-        name: "Vegetale",
-        quantity: 1,
-        unitPrice: 13,
-        totalPrice: 13,
-    },
-    {
-        pizzaId: 11,
-        name: "Spinach and Mushroom",
-        quantity: 1,
-        unitPrice: 15,
-        totalPrice: 15,
-    },
-];
+// const fakeCart = [
+//     {
+//         pizzaId: 12,
+//         name: "Mediterranean",
+//         quantity: 2,
+//         unitPrice: 16,
+//         totalPrice: 32,
+//     },
+//     {
+//         pizzaId: 6,
+//         name: "Vegetale",
+//         quantity: 1,
+//         unitPrice: 13,
+//         totalPrice: 13,
+//     },
+//     {
+//         pizzaId: 11,
+//         name: "Spinach and Mushroom",
+//         quantity: 1,
+//         unitPrice: 15,
+//         totalPrice: 15,
+//     },
+// ];
 
 function CreateOrder() {
     const navigation = useNavigate();
