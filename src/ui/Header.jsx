43c4c98d51f7206e4bfaa00842954814345ba227
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-03 09:34:32 +0330

    style texts

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom"
 import SearchOrder from "../features/order/SearchOrder"
+import Username from "../features/user/Username"
 
 function Header() {
     return (
-        <header className="bg-yellow-500">
-            <Link to='/'>Fast React Pizza Company</Link>
+        <header className="bg-yellow-500 uppercase">
+
+            <Link to='/' className="tracking-widest">Fast React Pizza Company</Link>
             <SearchOrder />
-            <p>Sina Hajipour</p>
+            <Username />
         </header>
     )
 }
