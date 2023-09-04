1ae3b761e19ff1c0773cb50db1f27274b11e4d6a
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-04 20:20:06 +0330

    add the cart page

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -1,13 +1,12 @@
 import { Link } from 'react-router-dom';
 
 function EmptyCart() {
-  return (
-    <div>
-      <Link to="/menu">&larr; Back to menu</Link>
-
-      <p>Your cart is still empty. Start adding some pizzas :)</p>
-    </div>
-  );
+    return (
+        <div className='py-3 px-4'>
+            <Link to="/menu">&larr; Back to menu</Link>
+            <p className='font-semibold mt-7'>Your cart is still empty. Start adding some pizzas :)</p>
+        </div>
+    );
 }
 
 export default EmptyCart;
