2171dc0f1989abfaad63ccd7aa68710aadaa1cc8
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-04 18:27:18 +0330

    config the custom button

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -2,34 +2,39 @@ import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
 
 function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+    // states
+    const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+    // methods
+    function handleAddToCart() {
+        console.log(id)
+    }
+    //ui
+    return (
+        <li className="flex gap-4 py-2">
+            <img
+                src={imageUrl}
+                alt={name}
+                className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
+            />
+            <div className="flex grow flex-col pt-0.5">
+                <p className="font-medium">{name}</p>
+                <p className="text-sm capitalize italic text-stone-500">
+                    {ingredients.join(', ')}
+                </p>
+                <div className="mt-auto flex items-center justify-between">
+                    {!soldOut ? (
+                        <p className="text-sm">{formatCurrency(unitPrice)}</p>
+                    ) : (
+                        <p className="text-sm font-medium uppercase text-stone-500">
+                            Sold out
+                        </p>
+                    )}
 
-  return (
-    <li className="flex gap-4 py-2">
-      <img
-        src={imageUrl}
-        alt={name}
-        className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
-      />
-      <div className="flex grow flex-col pt-0.5">
-        <p className="font-medium">{name}</p>
-        <p className="text-sm capitalize italic text-stone-500">
-          {ingredients.join(', ')}
-        </p>
-        <div className="mt-auto flex items-center justify-between">
-          {!soldOut ? (
-            <p className="text-sm">{formatCurrency(unitPrice)}</p>
-          ) : (
-            <p className="text-sm font-medium uppercase text-stone-500">
-              Sold out
-            </p>
-          )}
-
-          <Button type="small">Add to cart</Button>
-        </div>
-      </div>
-    </li>
-  );
+                    {!soldOut && <Button type="small" onClick={handleAddToCart}>Add to cart</Button>}
+                </div>
+            </div>
+        </li>
+    );
 }
 
 export default MenuItem;
