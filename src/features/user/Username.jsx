dd05488bea71065fbcb8feefb27cd615530ace95
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-04 09:57:31 +0330

    config redux

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,8 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
+    const username = useSelector(store => store.user.userName)
+    return <div className="hidden text-sm font-semibold md:block">{username}</div>;
 }
 
 export default Username;
