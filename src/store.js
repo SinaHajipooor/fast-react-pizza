fa60e13fa3ad459f1cb37e357c02c7f814a2b6c1
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-11-18 13:10:42 +0330

    add action create manually

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './features/user/userSlice'
 import cartReducer from './features/cart/cartSlice'
+import { createStore } from 'redux'
+
 
 const store = configureStore({
     reducer: {
@@ -27,10 +29,18 @@ function reducer(state = initialState, action) {
             return { ...state, balance: state.balance - action.payload }
         case 'account/requestLoan':
             if (state.loan > 0) return state;
-            return { ...state, loan: action.payload };
+            return { ...state, loan: action.payload.amount, loanPurpose: action.payload.purpose, balance: state.balance + action.payload.amount };
         case 'account/payLoan':
             return { ...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan }
         default:
             return state;
     }
 }
+
+
+const testStore = createStore(reducer);
+
+store.dispatch({ type: 'account/deposit', payload: 5000 });
+store.dispatch({ type: 'account/requestLoan', payload: { amount: 1000, purpose: 'buy a car' } })
+store.dispatch({ type: 'account/payLoan' })
+
